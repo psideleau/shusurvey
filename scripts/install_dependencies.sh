764fa33aab3af7b276e19a8cc368217df27ef66e
Jesse Daniels <danielsj5@mail.sacredheart.edu>
2019-05-24 22:21:06 -0400

    update bash scripts

--- scripts/install_dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-easy_install pip
-pip install -r /home/ec2-user/sample-app/requirements.txt
---
+++ src/scripts/install_dependencies.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+easy_install pip
+pip install -r /home/ec2-user/shusurvey/requirements.txt
