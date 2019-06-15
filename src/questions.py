eedfb5dfd67af4b6c4272d00e9099867b8667d7d
Jesse Daniels <1735941+fourtwizzy@users.noreply.github.com>
2019-06-15 19:52:20 -0400

    Update questions.py

--- src/questions.py
+++ src/questions.py
@@ -1,41 +1,26 @@
 class Question:
-     def __init__(self, prompt, answer, choices):
+    def __init__(self, prompt, choices):
           self.prompt = prompt
-          self.answer = answer
           self.choices = choices
 
-question_prompts = [
-     "\nWhat is your favorite type of food?\n(1)Asian\n(2)Italian\n(3)American\n(4)British\nAns: ",
-     "\nWhere in the world would you want to travel to?\n(1)South Korea\n(2)England\n(3)Japan\n(4)Germany\nAns: ",
-     "\nWhat dog would you want?\n(1)Yorkie\n(2)Husky\n(3)Australian Shepherd\n(4)Not a dog owner\nAns: ",
-     "\nWhat is your favorite sport to watch?\n(1)Football\n(2)Soccer\n(3)Tennis\n(4)Tae Kwon Do\nAns: ",
-     "\nWhat type of music to you like?\n(1)Rock and Roll\n(2)Country\n(3)K-Pop\n(4)Classical\nAns: ",
-     ]
+    def ask(self):
+        return '''
+            {}<br />
+           {} 
+        '''.format(self.prompt, self.get_options())
+
+    def get_options(self):
+        output = []
+        for choice in self.choices:
+            output.append('<input type="radio" name="choice" value="{}">{}<br />'.format(choice, choice))
+
+        return ''.join(output)
+            
 
 questions = [
-     Question(question_prompts[0], "1", [1,2,3,4]),
-     Question(question_prompts[1], "1", [1,2,3,4]),
-     Question(question_prompts[2], "1", [1,2,3,4]),
-     Question(question_prompts[3], "1", [1,2,3,4]),
-     Question(question_prompts[4], "1", [1,2,3,4]),
-     ]
-
-def run_quiz(questions):
-     score = 0
-     writefile = open ('SurveyAnswers.txt', 'a')
-     writefile.write(str(ans))
-     writefile.write("\n")
-     writefile.close()
-     for question in questions:
-          answer = input(question.prompt)
-          while not int(answer) in question.choices:
-               print("Invalid Option! Choose Again!")
-               answer = input(question.prompt)
-          writefile = open ('SurveyAnswers.txt', 'a')
-          writefile.write(str(answer))
-          writefile.write("\n")
-          writefile.close()
-          if answer == question.answer:
-               score += 1
-     print("\nThank You for Participating!")
-     print("You're answers will be sent to your email shortly.")
+    Question('What is your favorite type of food?', ['Asian', 'Italian', 'American', 'British']),
+    Question('Where in the world would you want to travel to?', ['South Korea', 'England', 'Japan', 'Germany']),
+    Question('What dog would you want?', ['Yorkie', 'Husky', 'Australian Shepherd', 'I want a cat']),
+    Question('What is your favorite sport to watch?', ['Football', 'Soccer', 'Tennis', 'Tae Kwon Do']),
+    Question('What type of music do you like?', ['Rock and Roll', 'Country', 'K-Pop', 'Classical', 'Post Rock']),
+]
